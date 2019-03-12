0be472fdcd8aba631847de7d8f641999ed5badad
Kaelan Patel <kaelanspatel@gmail.com>
2019-03-12 00:29:35 -0600

    preliminary annexation of all states into LUX

--- history/tagreplacer.py
+++ history/tagreplacer.py
@@ -1,7 +1,7 @@
 import os
 
 directory = 'states' # directory for files; script should be one level up from directory
-to_replace = 'SOV' # string to be replaced
+to_replace = 'YUN' # string to be replaced
 replace = 'LUX' # what to replace string to be replaced with
 
 def main():
