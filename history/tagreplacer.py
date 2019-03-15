498119a8b52985f0c32db9c14ba739575586ebdd
Kaelan Patel <kaelanspatel@gmail.com>
2019-03-15 14:28:03 -0600

    more SUE border cleanup

--- history/tagreplacer.py
+++ history/tagreplacer.py
@@ -1,8 +1,8 @@
 import os
 
 directory = 'states' # directory for files; script should be one level up from directory
-to_replace = 'POR' # string to be replaced
-replace = 'SPR' # what to replace string to be replaced with
+to_replace = 'AUS' # string to be replaced
+replace = 'GER' # what to replace string to be replaced with
 
 def main():
     for filename in os.listdir(directory):
