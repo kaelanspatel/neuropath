65263092d4c0f1f50ddd551469fdefba6b030ada
Kaelan Patel <kaelanspatel@gmail.com>
2019-04-08 20:39:32 -0600

    stop-gap fixes to 1990 start date

--- history/tagreplacer.py
+++ history/tagreplacer.py
@@ -1,16 +1,16 @@
 import os
 
 directory = 'states' # directory for files; script should be one level up from directory
-to_replace = 'IRQ' # string to be replaced
-replace = 'LEV' # what to replace string to be replaced with
+to_replace = '1940' # string to be replaced
+replace = '2500' # what to replace string to be replaced with
 
 def main():
     for filename in os.listdir(directory):
         lines = []
-        with open(directory + '/' + filename) as file:
+        with open(directory + '/' + filename, encoding="utf8") as file:
             for line in file:
                 lines.append(line.replace(to_replace, replace))
-        with open(directory + '/' + filename, 'w') as f:
+        with open(directory + '/' + filename, 'w', encoding="utf8") as f:
             for line in lines:
                 f.write(line)
 
